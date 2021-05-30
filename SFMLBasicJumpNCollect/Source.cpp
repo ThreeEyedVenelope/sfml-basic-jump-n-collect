3420db68bdeb82653f7ad4adc1764fd8d99d0738
Cattatonicat <threeeyedvenelope@gmail.com>
2021-05-29 20:32:52 -0500

    Create SFML window

--- SFMLBasicJumpNCollect/Source.cpp
+++ SFMLBasicJumpNCollect/Source.cpp
@@ -1,7 +1,18 @@
-
+#include <SFML/Graphics.hpp>
 #include <iostream>
+
+sf::Vector2f windowSize(800, 800);
+sf::VideoMode vm(windowSize.x, windowSize.y);
+sf::RenderWindow window(vm, "Basic Jump N Collect");
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    while (window.isOpen()) {
+        sf::Event event; 
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
 }
