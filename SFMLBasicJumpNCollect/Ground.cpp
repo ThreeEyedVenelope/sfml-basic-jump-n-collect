62c2dedbe9d3038a3221c555231b3947b887855a
Cattatonicat <threeeyedvenelope@gmail.com>
2021-05-29 21:46:51 -0500

    Add environment

---
+++ SFMLBasicJumpNCollect/Ground.cpp
@@ -0,0 +1,12 @@
+#include "Ground.h"
+
+Ground::Ground(sf::Vector2f windowSize) {
+	m_texture.loadFromFile("graphics/Groundx8.png");
+	m_sprite.setTexture(m_texture);
+	m_sprite.setOrigin(m_texture.getSize().x / 2, m_texture.getSize().y / 2);
+	m_sprite.setPosition(windowSize.x / 2, windowSize.y - m_texture.getSize().y / 2);
+}
+
+sf::Sprite Ground::getSprite() {
+	return m_sprite;
+}
--- SFMLBasicJumpNCollect/Source.cpp
+++ SFMLBasicJumpNCollect/Source.cpp
@@ -1,12 +1,26 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "Ground.h"
 
 sf::Vector2f windowSize(800, 800);
 sf::VideoMode vm(windowSize.x, windowSize.y);
 sf::RenderWindow window(vm, "Basic Jump N Collect");
 
+sf::Texture backgroundTexture;
+sf::Sprite backgroundSprite;
+
+Ground ground(windowSize);
+
+void setBackground() {
+    backgroundTexture.loadFromFile("graphics/OrangeBackgroundx8.png");
+    backgroundSprite.setTexture(backgroundTexture);
+    backgroundSprite.setPosition(0, 0);
+}
+
 int main()
 {
+    setBackground();
+
     while (window.isOpen()) {
         sf::Event event; 
         while (window.pollEvent(event)) {
@@ -14,5 +28,10 @@ int main()
                 window.close();
             }
         }
+        window.clear();
+        window.draw(backgroundSprite);
+        window.draw(ground.getSprite());
+        window.display();
     }
+    return 0;
 }
